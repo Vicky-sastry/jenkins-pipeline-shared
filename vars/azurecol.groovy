c41f5f6686ef9c7d3a174b2094ef02a72adc4c6f
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-01-24 14:42:27 +0530

    Create azurecol.groovy

---
+++ vars/azurecol.groovy
@@ -0,0 +1,4 @@
+def call()
+{
+  sh 'curl -u vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya --request GET https://dev.azure.com/vickysastryvs/_apis/projects?api-version=5.1'
+}
