126fdf8a404513c409e5f7fb7c6697ac8be5d118
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-01-24 18:34:41 +0530

    Update azureconn.groovy

--- vars/azureconn.groovy
+++ vars/azureconn.groovy
@@ -6,7 +6,7 @@ def call()
 --header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' \
 --data-raw '{
-  "name": "Finalone",
+  "name": "Finaltwo",
   "description": "Testing for Project creation using curl",
   "capabilities": {
     "versioncontrol": {
