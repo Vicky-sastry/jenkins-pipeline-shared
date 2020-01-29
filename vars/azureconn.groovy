f104ee5927bd3adde4199bb5bb09caa377b204d8
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-01-29 18:04:33 +0530

    Update azureconn.groovy

--- vars/azureconn.groovy
+++ vars/azureconn.groovy
@@ -2,7 +2,7 @@ def call(name){
 
   echo "My project name is ${name}"
   sh '''
-  export name = "name"
+  export  myvar == "name",
 
  echo "My project name is ${name}"
                 curl --location --request POST 'https://dev.azure.com/vickysastryvs/_apis/projects?api-version=6.0-preview.4' \
@@ -10,7 +10,7 @@ def call(name){
 --header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' \
 --data-raw '{
-  "name": ${name},
+  "name": "${name}",
   "description": "Testing for Project creation using curl",
   "capabilities": {
     "versioncontrol": {
