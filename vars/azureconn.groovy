fe36c62ac097447532d05061dd8419d2b9f1b324
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-01-29 17:14:20 +0530

    Update azureconn.groovy

--- vars/azureconn.groovy
+++ vars/azureconn.groovy
@@ -1,6 +1,6 @@
 def call(name){
 
-  sh 'echo ${name}'
+  sh 'echo "${name}"'
   sh '''
                 curl --location --request POST 'https://dev.azure.com/vickysastryvs/_apis/projects?api-version=6.0-preview.4' \
 --header 'Content-Type: application/json' \
