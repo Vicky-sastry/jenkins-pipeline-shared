3a92c658a24164290cf8057c564017c0f3ae4dc0
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-01-29 17:36:41 +0530

    Update azureconn.groovy

--- vars/azureconn.groovy
+++ vars/azureconn.groovy
@@ -7,7 +7,7 @@ def call(name){
 --header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' \
 --data-raw '{
-  "name": ${name},
+  "name": '${name}',
   "description": "Testing for Project creation using curl",
   "capabilities": {
     "versioncontrol": {
