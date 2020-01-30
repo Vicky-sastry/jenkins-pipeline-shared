d865d3d84697b68233453404a416606dd3993f20
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-01-30 15:38:43 +0530

    Update azureconn.groovy

--- vars/azureconn.groovy
+++ vars/azureconn.groovy
@@ -27,7 +27,7 @@ def projectName = resultJson.key
 def call(){
  def request = libraryResource 'data.json'
  createProject(request)
-   echo 'The project "$projectName" is created'
+   echo 'The project "${projectName}" is created'
 }
 
 
