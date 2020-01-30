1cfedaebcc5daf5c044b1bf842ff1719d75589ee
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-01-30 14:32:45 +0530

    Update azureconn.groovy

--- vars/azureconn.groovy
+++ vars/azureconn.groovy
@@ -33,7 +33,7 @@ def call(){
  def call(name){
   echo "My project name is ${name}"
 
-            sh '''    curl --location --request POST 'https://dev.azure.com/vickysastryvs/_apis/projects?api-version=6.0-preview.4' \
+            sh """    curl --location --request POST 'https://dev.azure.com/vickysastryvs/_apis/projects?api-version=6.0-preview.4' \
 --header 'Content-Type: application/json' \
 --header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' \
@@ -48,6 +48,6 @@ def call(){
       "templateTypeId": "6b724908-ef14-45cf-84f8-768b5384da45"
     }
   }
-}''''
+}"""
 }
 
