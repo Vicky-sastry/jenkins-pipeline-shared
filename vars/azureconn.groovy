884fc972a260c746d0a829c50949e8bd6078c45f
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-01-31 14:32:23 +0530

    Update azureconn.groovy

--- vars/azureconn.groovy
+++ vars/azureconn.groovy
@@ -19,8 +19,7 @@ def projectName = resultJson.key
       "templateTypeId": "6b724908-ef14-45cf-84f8-768b5384da45"
     }
   }
-}""", 
-   /*responseHandle: 'NONE',*/ url: "https://dev.azure.com/vickysastryvs/_apis/projects?api-version=5.1"
+}""",  url: "https://dev.azure.com/vickysastryvs/_apis/projects?api-version=5.1"
 
  
 }
