5cce8d48a59568e063e34ae7241d1e85f6bc125c
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-05 14:51:29 +0530

    Create azdelete.groovy

---
+++ vars/azdelete.groovy
@@ -0,0 +1,17 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+deleteProject(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def projectid = resultJson.id 
+   httpRequest authentication: 'azurecred', 
+    customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
+                    [maskValue: false, name: 'Accept', value: 'application/json']], 
+    httpMode: 'DELETE', url: "https://dev.azure.com/vickysastryvs/_apis/projects/${projectid}?api-version=5.1"
+}
+def call(){
+ def request = libraryResource '/var/lib/jenkins/workspace/azure/output.json'
+ deleteProject(request)
+   //echo "The project ${projectid} is deleted"
+}
