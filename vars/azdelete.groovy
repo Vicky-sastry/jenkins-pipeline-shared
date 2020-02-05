f811733039bd03bbb9797f00abf316e872d8738e
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-05 15:54:57 +0530

    Update azdelete.groovy

--- vars/azdelete.groovy
+++ vars/azdelete.groovy
@@ -12,7 +12,7 @@ def projectid = resultJson.id
 }
 def call(){
  //def filePath = readFile "${WORKSPACE}/var/lib/jenkins/workspace/azd/output.json"
-   def props = readJSON file: '/output.json'
+   def props = readJSON file: '${WORKSPACE}/var/lib/jenkins/workspace/azd/output.json'
  def request = libraryResource 'props'
  deleteProject(request)
    //echo "The project ${projectid} is deleted"
