39c26bfdb6186402f63645674a11b76797f62efe
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-05 14:40:30 +0530

    Create create_project_json.groovy

---
+++ vars/create_project_json.groovy
@@ -0,0 +1,30 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.environments)
+
+String a=jsonObj.environments.deploy.key
+//String a=jsonObj.alm.projects.project.name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+env.name = projectName
+
+sh """    
+            echo "My project name is ${name}"
+            curl --location --request POST 'https://dev.azure.com/vickysastryvs/_apis/projects?api-version=6.0-preview.4' \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+--header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' \
+--data-raw '{
+  "name": "${projectName}",
+  "description": "Testing for Project creation using curl",
+  "capabilities": {
+    "versioncontrol": {
+      "sourceControlType": "Git"
+    },
+    "processTemplate": {
+      "templateTypeId": "6b724908-ef14-45cf-84f8-768b5384da45"
+    }
+  }
+}"""
+}
