c53d60fafabe140b03493845f2115ec802e747c9
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-06 14:52:28 +0530

    Create fetch_teams_org.groovy

---
+++ vars/fetch_teams_org.groovy
@@ -0,0 +1,18 @@
+def call(jsondata){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+println(jsonObj.environments.environment)
+
+String a=jsonObj.environments.environment.deploy.organization
+String orgname=a.replaceAll("\\[", "").replaceAll("\\]","");
+env.name = orgname
+}
+sh """
+curl --location --request GET 'https://dev.azure.com/${orgname}/_apis/teams?api-version=5.1-preview.3' \
+--header 'Accept: application/json' \
+--header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ=='
+
+-H 'content-type: application/json'  | json_reformat
+"""
+}
+
