5979da304d9474afdd44c9a968621a3460ed7821
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-07 11:35:35 +0530

    Update logfun.groovy

--- vars/logfun.groovy
+++ vars/logfun.groovy
@@ -1,6 +1,10 @@
-def call(message)
+def call(message,jsondata)
 {
+ String a=jsonObj.environments.environment.deploy.key
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+env.name = projectName
+
 // println(message)
   Date date = new Date() 
-  sh " echo '${date}' AZURE ${message} >>log.txt"
+  sh " echo '${date}' AZURE project with ${projectName} ${message} >>log.txt"
 }
