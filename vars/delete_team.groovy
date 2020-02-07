36779a3c509c1dbcec13823e86868c31cac19e41
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-07 12:29:33 +0530

    Create delete_team.groovy

---
+++ vars/delete_team.groovy
@@ -0,0 +1,18 @@
+import groovy.json.*
+
+@NonCPS
+delete_team(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/azure/team.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def teamid = resultJson.value[0].id
+
+  sh """ 
+    curl --location --request DELETE 'https://dev.azure.com/vickysastryvs/_apis/projects/${projectid}/teams/${teamid}?api-version=5.1' \
+--header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' 
+  """
+}
+def call()
+{
+delete_team()
+}
