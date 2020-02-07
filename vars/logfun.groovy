87556026a2600e59cc00c5f6bb5ccd5c3f535cba
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-07 11:17:14 +0530

    Update logfun.groovy

--- vars/logfun.groovy
+++ vars/logfun.groovy
@@ -1,13 +1,6 @@
-import groovy.json.JsonSlurper 
-@NonCPS
-
 def call(message)
 {
  println(message)
- def request = libraryResource 'data.json'
- def jsonSlurper = new JsonSlurper() 
- def resultJson = jsonSlurper.parseText(request)
- def projectName = resultJson.key
   Date date = new Date() 
-  sh " echo '${date}' Azure project with the projectname '${projectName}' ${message} >>log.txt"
+  sh " echo '${date}' AZURE ${message} >>log.txt"
 }
