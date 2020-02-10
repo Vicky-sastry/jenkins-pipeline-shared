3712eff7f468a8b6285b6d3d61b6b74bd356a0cd
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-10 14:29:10 +0530

    Update delete_proj_json.groovy

--- vars/delete_proj_json.groovy
+++ vars/delete_proj_json.groovy
@@ -3,16 +3,23 @@ import groovy.json.*
 @NonCPS
 deleteProject(){
 def jsonSlurper = new JsonSlurper() 
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/azure/out.json"),"UTF-8"))
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/out.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def projectid = resultJson.id 
    sh """
-   curl --location --request DELETE 'https://dev.azure.com/vickysastryvs/_apis/projects/${projectid}?api-version=5.1' \
+   curl --location --request DELETE 'https://dev.azure.com/${orgname}/_apis/projects/${projectid}?api-version=5.1' \
 --header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ=='
 """
 }
 
 def call(jsondata){
- deleteProject()
+   
+def jsonString = jsondata
+
+def jsonObj = readJSON text: jsonString
+String b=jsonObj.environments.environment.deploy.organization
+String orgname=b.replaceAll("\\[", "").replaceAll("\\]","");
+
+ deleteProject(orgname)
 }
