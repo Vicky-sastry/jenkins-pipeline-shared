b6072e3b30d646132badfdd2eb8222c62faeae74
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-10 14:23:22 +0530

    Update create_team_json.groovy

--- vars/create_team_json.groovy
+++ vars/create_team_json.groovy
@@ -1,13 +1,13 @@
 import groovy.json.*
 
 @NonCPS
-create(String team_name){
+create(String team_name, String orgname){
 def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/azure/out.json"),"UTF-8"))
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/out.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def projectId = resultJson.id
 	sh """
-            curl --location --request POST 'https://dev.azure.com/vickysastryvs/_apis/projects/${projectId}/teams?api-version=5.1' \
+            curl --location --request POST 'https://dev.azure.com/${orgname}/_apis/projects/${projectId}/teams?api-version=5.1' \
 --header 'Accept: application/json' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' \
@@ -26,6 +26,9 @@ def jsonObj = readJSON text: jsonString
 
 String a = jsonObj.environments.environment.deploy.team_name
 String team_name=a.replaceAll("\\[", "").replaceAll("\\]","");
-create(team_name)
+String b=jsonObj.environments.environment.deploy.organization
+String orgname=b.replaceAll("\\[", "").replaceAll("\\]","");
+create(team_name,orgname)
+
 
 }
