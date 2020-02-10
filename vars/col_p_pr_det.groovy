94a3c4a4423565f1bd5fb1ff157047b068d621dc
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-10 12:51:22 +0530

    Update col_p_pr_det.groovy

--- vars/col_p_pr_det.groovy
+++ vars/col_p_pr_det.groovy
@@ -1,13 +1,13 @@
 import groovy.json.*
 
 @NonCPS
-create(String projectName){
+create(String orgname){
 def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/azure/out.json"),"UTF-8"))
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/out.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def projectId = resultJson.id
 sh """
-curl --location --request GET 'https://dev.azure.com/vickysastryvs/_apis/projects/${projectId}?api-version=5.1' \
+curl --location --request GET 'https://dev.azure.com/${orgname}/_apis/projects/${projectId}?api-version=5.1' \
 --header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ=='
 -H 'content-type: application/json'  | json_reformat
@@ -21,5 +21,8 @@ println(jsonObj.environments.environment)
 
 String a=jsonObj.environments.environment.deploy.key
 String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
-env.name = projectName
+
+String b=jsonObj.environments.environment.deploy.organization
+String orgname=b.replaceAll("\\[", "").replaceAll("\\]","");
+env.name = orgname
 }
