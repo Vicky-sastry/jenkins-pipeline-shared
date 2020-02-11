37c14d57bcc00558e31c9e03b6fd003f9567b661
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-11 18:18:16 +0530

    Update fetchcommits.groovy

--- vars/fetchcommits.groovy
+++ vars/fetchcommits.groovy
@@ -8,7 +8,7 @@ String orgname=a.replaceAll("\\[", "").replaceAll("\\]","");
 env.name = orgname
 
 sh """ 
-curl --location --request GET 'https://dev.azure.com/${orgname}/d2/_apis/build/builds?api-version=5.1' \
+curl --location --request GET 'https://dev.azure.com/vickysastryvs/d2/_apis/git/repositories/d2/pushes?api-version=5.1' \
 --header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ=='
 """
