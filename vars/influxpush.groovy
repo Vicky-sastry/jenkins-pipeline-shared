a4c403e6dcafb79f1a74642b26864c39a0267dbb
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-11 20:02:05 +0530

    Update influxpush.groovy

--- vars/influxpush.groovy
+++ vars/influxpush.groovy
@@ -3,11 +3,11 @@ import groovy.json.*
 @NonCPS
 create(){
 def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/out.json"),"UTF-8"))
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/commit.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
-def total = resultJson.
-  echo "$total"
-sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'azurerepo,pushes=${count},commits=${count},pullrequests=${count}'
+def commitcount = resultJson.count
+echo "$count"
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'azurerepo,commits=${count}'
 """
 }
 
