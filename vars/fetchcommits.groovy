273ff230166426a126120ef4abc99d12dd463dba
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-11 18:12:14 +0530

    Update fetchcommits.groovy

--- vars/fetchcommits.groovy
+++ vars/fetchcommits.groovy
@@ -7,7 +7,8 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def projectId = resultJson.id
 sh """
-curl --location --request GET 'https://dev.azure.com/vickysastryvs/d2/_apis/git/repositories/d2/commits?api-version=5.1' \
+curl --location --request GET 'https://dev.azure.com/vickysastryvs/d2/_apis/git/repositories/d2/pushes?api-version=5.1' \
+--header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ=='
 """
 }
