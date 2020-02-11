7a4a826bc71d295cd0bd8128bdc3f23d1aa896a1
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-11 18:03:16 +0530

    Update fetchcommits.groovy

--- vars/fetchcommits.groovy
+++ vars/fetchcommits.groovy
@@ -1,13 +1,13 @@
 import groovy.json.*
 
 @NonCPS
-create(String orgname){
+create(String orgname, String projectName){
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/out.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def projectId = resultJson.id
 sh """
-curl --location --request GET 'https://dev.azure.com/{orgname}/${projectName}/_apis/git/pullrequests?api-version=5.1' \
+curl --location --request GET 'https://dev.azure.com/${orgname}/${projectName}/_apis/git/pullrequests?api-version=5.1' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ=='
 
 """
@@ -23,5 +23,4 @@ String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
 
 String b=jsonObj.environments.environment.deploy.organization
 String orgname=b.replaceAll("\\[", "").replaceAll("\\]","");
-env.name = orgname
 }
