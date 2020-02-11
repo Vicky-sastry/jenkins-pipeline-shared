c692d61e9c6a12e19a2758455a06f98ef2b4c037
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-11 19:38:04 +0530

    Update storeoutput.groovy

--- vars/storeoutput.groovy
+++ vars/storeoutput.groovy
@@ -10,8 +10,8 @@ String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
 env.name = projectName
 
  sh """
-       curl  --request GET 'https://dev.azure.com/vickysastryvs/_apis/projects/${projectName}?api-version=5.1' \
-             --header 'Accept: application/json' \
-             --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' -o out.json
+      curl --location --request GET 'https://dev.azure.com/${orgname}/${projectName}/_apis/git/repositories/${projectName}/commits?api-version=5.1' \
+--header 'Accept: application/json' \
+--header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' -o commit.json
     """
 }
