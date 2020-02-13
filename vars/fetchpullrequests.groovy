9e3b5d0f8f52b66fd2fb7b64d8285409da302876
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-13 17:25:30 +0530

    Update fetchpullrequests.groovy

--- vars/fetchpullrequests.groovy
+++ vars/fetchpullrequests.groovy
@@ -1,30 +1,21 @@
 import groovy.json.*
-
-@NonCPS
-create(String orgname){
-def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/pullrequests.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-def pullrequestcount = resultJson.count
-
-  sh """ 
-curl --location --request GET 'https://dev.azure.com/${orgname}/${projectName}/_apis/git/pullrequests?api-version=5.1' \
---header 'Accept: application/json' \
---header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' -o pullrequests.json
-"""
-}
-
+  
 def call(jsondata){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 println(jsonObj.environments.environment)
-
+  
 String a=jsonObj.environments.environment.deploy.organization
 String orgname=a.replaceAll("\\[", "").replaceAll("\\]","");
-env.name = orgname
 
 String b=jsonObj.environments.environment.deploy.key
 String projectName=b.replaceAll("\\[", "").replaceAll("\\]","");
+  
+   sh """ 
+curl --location --request GET 'https://dev.azure.com/${orgname}/${projectName}/_apis/git/pullrequests?api-version=5.1' \
+--header 'Accept: application/json' \
+--header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' -o pullrequests.json
+"""
 
 
 }
