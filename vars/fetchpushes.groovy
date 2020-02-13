ea6f7b89e8f1ea816d7521f76c8fe7e70a3a6673
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-13 17:22:33 +0530

    Update fetchpushes.groovy

--- vars/fetchpushes.groovy
+++ vars/fetchpushes.groovy
@@ -7,13 +7,6 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def pushcount = resultJson.count*/
   
-  sh """ 
-curl --location --request GET 'https://dev.azure.com/${orgname}/${projectName}/_apis/git/repositories/${projectName}/pushes?api-version=5.1' \
---header 'Accept: application/json' \
---header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' -o pushes.json
-"""
-}
-
 
 def call(jsondata){
 def jsonString = jsondata
@@ -26,6 +19,12 @@ env.name = orgname
 
 String b=jsonObj.environments.environment.deploy.key
 String projectName=b.replaceAll("\\[", "").replaceAll("\\]","");
+  
+   sh """ 
+curl --location --request GET 'https://dev.azure.com/${orgname}/${projectName}/_apis/git/repositories/${projectName}/pushes?api-version=5.1' \
+--header 'Accept: application/json' \
+--header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' -o pushes.json
+"""
 
 
 }
