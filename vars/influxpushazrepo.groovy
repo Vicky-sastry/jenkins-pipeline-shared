aaa3428415d8e0707a31abb9bde413aa4565414f
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-13 17:18:41 +0530

    Update influxpushazrepo.groovy

--- vars/influxpushazrepo.groovy
+++ vars/influxpushazrepo.groovy
@@ -7,11 +7,16 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def count = resultJson.count
   
-  def read = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/pullrequests.json"),"UTF-8"))
+def read = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/pullrequests.json"),"UTF-8"))
 def result = jsonSlurper.parse(read)
 def pullreqcount = result.count
-echo "$count, $pullreqcount"
-sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'azurerepo commits=${count},pullrequests=${pullreqcount}'
+
+def reader2 = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/pushes.json"),"UTF-8"))
+def result2 = jsonSlurper.parse(reader2)
+def pushcount = result2.count
+  
+echo "$count, $pullreqcount, $pushcount"
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'azurerepo commits=${count},pullrequests=${pullreqcount},pushes=${pushcount}'
 """
 }
 
