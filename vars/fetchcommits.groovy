b245deb13b48378c757018eb055306f5f00b2f1c
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-14 13:01:15 +0530

    Update fetchcommits.groovy

--- vars/fetchcommits.groovy
+++ vars/fetchcommits.groovy
@@ -7,7 +7,7 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def commitcount = resultJson.count
  sh """ 
-Ocurl --location --request GET 'https://dev.azure.com/${orgname}/${projectName}/_apis/git/repositories/${projectName}/commits?api-version=5.1' --data asOfDate='"20200212"' \
+Ocurl --location --request GET 'https://dev.azure.com/${orgname}/${projectName}/_apis/git/repositories/${projectName}/commits?api-version=5.1' --data asOfDate="20200212" \
 --header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ=='
 """
