f9db75b946be8387e20278811ec195fb4863ae8b
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-14 16:47:43 +0530

    Update fetchpullrequests.groovy

--- vars/fetchpullrequests.groovy
+++ vars/fetchpullrequests.groovy
@@ -12,7 +12,7 @@ String b=jsonObj.environments.environment.deploy.key
 String projectName=b.replaceAll("\\[", "").replaceAll("\\]","");
   
    sh """ 
-curl --request GET 'https://dev.azure.com/${orgname}/${projectName}/_apis/git/pullrequests?timeRange=14/02/2020+11:00~14/02/2020+12:00&timeUnit=day&api-version=5.1' \
+curl --request GET 'https://dev.azure.com/${orgname}/${projectName}/_apis/git/pullrequests?timeRange=14/02/2020%2011:00~14/02/2020%2012:00&timeUnit=day&api-version=5.1' \
 --header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' -o pullrequests.json
 """
