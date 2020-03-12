cf45cd132094c76a885b16a7e43ed949e693e891
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-03-12 18:42:12 +0530

    Update fetchprojects.groovy

--- vars/fetchprojects.groovy
+++ vars/fetchprojects.groovy
@@ -4,7 +4,7 @@ import groovy.json.*
 create(String orgname){
   sh """
  curl --location --request GET 'https://dev.azure.com/${orgname}/_apis/projects?api-version=5.0%0A' \
---header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206eDIyYXpoejRweHBzbmltMjJod295dzJkNG9xdjZtbzJ3czRsemgyNzZpc2trdW5ueXR5YQ=='-o projectlist.json
+--header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206eDIyYXpoejRweHBzbmltMjJod295dzJkNG9xdjZtbzJ3czRsemgyNzZpc2trdW5ueXR5YQ==' -o projectlist.json
 """
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/projectlist.json"),"UTF-8"))
